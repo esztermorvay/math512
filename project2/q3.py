136ed57a6688b24ecabc75a17d51d7a18dd7c06d
Eszter Morvay <morvay@usc.edu>
2023-02-06 01:03:21 -0800

    add pickled results

--- project2/q3.py
+++ project2/q3.py
@@ -1,8 +1,9 @@
+import pickle
 import random
 
 import numpy as np
-from matplotlib import pyplot as plt
-from ncephes import cprob
+# from matplotlib import pyplot as plt
+from ncephes import bdtri
 
 def bernoulli(n, p):
     # simulate n trials of a bernoulli trial with probability p and adds the sum
@@ -10,10 +11,25 @@ def bernoulli(n, p):
 
 def partB():
     results = []
+    results2 = []
     for i in range(0, 6000):
         successes = 0
         for i in range(100):
             num = random.uniform(0,1)
+            if num <= .7:
+                successes += 1
+        # use the inverse binomial cdf from cprob
+        result = bdtri(successes, 100, .7)
+        results.append(result)
+    print(results)
+    # pickle results
+    pickle.dump(results, open("results.p", "wb"))
+    # plot a histogram of results
+    # plt.hist(results, bins=15)
+    # plt.title("Binomial Distribution Using Inverse Binomial CDF")
+    # plt.xlabel("Sum of Successes")
+    # plt.ylabel("Frequency")
+
 def partA():
     # 6000 trials with prob .7
     results = []
@@ -35,7 +51,7 @@ def partA():
     print("Proportion of trials with less than or equal to 70 successes: ", count / 6000)
 
 def main():
-    partA()
+    partB()
 if __name__ == "__main__":
     main()
 
