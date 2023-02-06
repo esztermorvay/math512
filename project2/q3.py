a34f31eb4a2b2debf37ac92ea9cb1d36585c9521
Eszter Morvay <morvay@usc.edu>
2023-02-06 11:10:02 -0800

    add histogram

--- project2/q3.py
+++ project2/q3.py
@@ -2,33 +2,38 @@ import pickle
 import random
 
 import numpy as np
-# from matplotlib import pyplot as plt
-from ncephes import bdtri
+from matplotlib import pyplot as plt
+# from ncephes import bdtri
 
 def bernoulli(n, p):
     # simulate n trials of a bernoulli trial with probability p and adds the sum
     return np.random.binomial(n, p)
 
 def partB():
-    results = []
-    results2 = []
-    for i in range(0, 6000):
-        successes = 0
-        for i in range(100):
-            num = random.uniform(0,1)
-            if num <= .7:
-                successes += 1
-        # use the inverse binomial cdf from cprob
-        result = bdtri(successes, 100, .7)
-        results.append(result)
-    print(results)
+    # results = []
+    # results2 = []
+    # for i in range(0, 6000):
+    #     successes = 0
+    #     for i in range(100):
+    #         num = random.uniform(0,1)
+    #         if num <= .7:
+    #             successes += 1
+    #     # use the inverse binomial cdf from cprob
+    #     result = bdtri(successes, 100, .7)
+    #     results.append(result)
+    # print(results)
     # pickle results
-    pickle.dump(results, open("results.p", "wb"))
-    # plot a histogram of results
-    # plt.hist(results, bins=15)
-    # plt.title("Binomial Distribution Using Inverse Binomial CDF")
-    # plt.xlabel("Sum of Successes")
-    # plt.ylabel("Frequency")
+    # pickle.dump(results, open("results.p", "wb"))
+    #unpickle
+    results = pickle.load(open("results.p", "rb"))
+    # multiply everythin in results by 100
+    for i in range(len(results)):
+        results[i] *= 100
+    plt.hist(results, bins=17)
+    plt.title("Binomial Distribution Using Inverse Binomial CDF")
+    plt.xlabel("Successes")
+    plt.ylabel("Frequency")
+    plt.savefig("q3b.png")
 
 def partA():
     # 6000 trials with prob .7
