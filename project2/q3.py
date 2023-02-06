95d670bb1dcee656582392317393e6abfcd071da
Eszter Morvay <morvay@usc.edu>
2023-02-06 00:01:01 -0800

    import

--- project2/q3.py
+++ project2/q3.py
@@ -1,13 +1,20 @@
+import random
+
 import numpy as np
 from matplotlib import pyplot as plt
-
+from ncephes import cprob
 
 def bernoulli(n, p):
     # simulate n trials of a bernoulli trial with probability p and adds the sum
     return np.random.binomial(n, p)
 
-
-def main():
+def partB():
+    results = []
+    for i in range(0, 6000):
+        successes = 0
+        for i in range(100):
+            num = random.uniform(0,1)
+def partA():
     # 6000 trials with prob .7
     results = []
     for i in range(0, 6000):
@@ -27,6 +34,8 @@ def main():
             count += 1
     print("Proportion of trials with less than or equal to 70 successes: ", count / 6000)
 
+def main():
+    partA()
 if __name__ == "__main__":
     main()
 
